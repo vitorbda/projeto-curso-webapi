52c5fbe9cc8d46b7379302b94e4d3ca600daab5a
Joao V B Andrade <80553392+vitorbda@users.noreply.github.com>
2024-10-19 11:59:39 -0300

    [S3-A27] - Criação do Domain e classe Categoria.cs

--- APICatalogo/APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d074586549e6f8bba626987ce3b5dda3126e152f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37dc579d44861e175bab57b56995a360dfdf5521")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
