5507bd39d87881bcb4388a4d77ac8b7bf3f9fc1f
Joao V B Andrade <80553392+vitorbda@users.noreply.github.com>
2024-10-21 15:37:06 -0300

    [S3-A28] - Inclui pacote EF Design

--- APICatalogo/APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ef55a129a10498380b787099cd9bc1e796776e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3906b4f24639415cb21cf86a4cc3038d51f5c9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
