bc244e93a12a63c9c8a3c90fc860b37597172340
Joao V B Andrade <80553392+vitorbda@users.noreply.github.com>
2024-10-31 07:26:48 -0300

    [S4-A60] Validação personalizada I

--- APICatalogo/APICatalogo/Models/Produto.cs
+++ APICatalogo/APICatalogo/Models/Produto.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using APICatalogo.Validations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
@@ -11,22 +12,23 @@ namespace APICatalogo.Models
         public int Id { get; set; }
         [Required(ErrorMessage = "O nome é obrigatório")]
         [StringLength(80, ErrorMessage = "O nome deve ter entre 5 e 20 caracteres", MinimumLength = 5)]
-        public string? Nome { get; set; }
+        [PrimeiraLetraMaiuscula]
+        public string Nome { get; set; }
         [Required]
         [StringLength(300)]
-        public string? Descricao { get; set; }
+        public string Descricao { get; set; }
         [Required]
         [Column(TypeName = "decimal(10,2)")]
         [Range(1, 10000, ErrorMessage = "O preço deve estar entre {1} e {2}")]
         public decimal Preco { get; set; }
         [Required]
         [StringLength(300)]
-        public string? ImagemUrl { get; set; }
+        public string ImagemUrl { get; set; }
         public float Estoque { get; set; }
         public DateTime DataCadastro { get; set; }
         public int CategoriaId { get; set; }
 
         [JsonIgnore]
-        public Categoria? Categoria { get; set; }
+        public Categoria Categoria { get; set; }
     }
 }
