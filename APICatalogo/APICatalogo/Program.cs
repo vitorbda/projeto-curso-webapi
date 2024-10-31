a4284a0381dee200ab6608a0ed14eea02f988670
Joao V B Andrade <80553392+vitorbda@users.noreply.github.com>
2024-10-31 09:03:21 -0300

    [S4-A65] Middleware - Realizando o tratamento global de exceções

--- APICatalogo/APICatalogo/Program.cs
+++ APICatalogo/APICatalogo/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using APICatalogo.Context;
 using System.Text.Json.Serialization;
 using APICatalogo.Services;
+using APICatalogo.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -32,6 +33,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.ConfigureExceptionHandler();
 }
 
 app.UseHttpsRedirection();
