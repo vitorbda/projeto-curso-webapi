bd253430856a0bd46b8fc29c0a59813e8ec77be5
Joao V B Andrade <80553392+vitorbda@users.noreply.github.com>
2024-10-31 07:39:17 -0300

    [S4-A61] Validação personalizada II

--- APICatalogo/APICatalogo/Models/Produto.cs
+++ APICatalogo/APICatalogo/Models/Produto.cs
@@ -1,4 +1,5 @@
 ﻿using APICatalogo.Validations;
+using Newtonsoft.Json.Linq;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
@@ -6,13 +7,13 @@ using System.Text.Json.Serialization;
 namespace APICatalogo.Models
 {
     [Table("Produto")]
-    public class Produto
+    public class Produto : IValidatableObject
     {
         [Key]
         public int Id { get; set; }
         [Required(ErrorMessage = "O nome é obrigatório")]
         [StringLength(80, ErrorMessage = "O nome deve ter entre 5 e 20 caracteres", MinimumLength = 5)]
-        [PrimeiraLetraMaiuscula]
+        //[PrimeiraLetraMaiuscula]
         public string Nome { get; set; }
         [Required]
         [StringLength(300)]
@@ -30,5 +31,23 @@ namespace APICatalogo.Models
 
         [JsonIgnore]
         public Categoria Categoria { get; set; }
+
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(this.Nome)) 
+            {
+                var primeiraLetra = this.Nome[0].ToString();
+                if (primeiraLetra != primeiraLetra.ToUpper())
+                {
+                    yield return new ValidationResult("A primeira letra do produto deve ser maiúscula", new[] { nameof(this.Nome) });
+                }
+            }
+
+            if (this.Estoque <= 0)
+            {
+                yield return new ValidationResult("O estoque deve ser maior que zero", new[] { nameof(this.Estoque) });
+            }
+        }
     }
 }
