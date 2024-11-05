810e7ded9a1c32e6e76b85b2894bf576395c14e8
Joao V B Andrade <80553392+vitorbda@users.noreply.github.com>
2024-11-05 08:45:24 -0300

    [S9-A132] Criação do controlador AuthController para os serviços de autenticação

---
+++ APICatalogo/APICatalogo/Controller/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using APICatalogo.Models;
+using APICatalogo.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ITokenService _tokenService;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _config;
+
+        public AuthController(ITokenService tokenService, 
+            UserManager<ApplicationUser> userManager, 
+            RoleManager<IdentityRole> roleManager, 
+            IConfiguration config)
+        {
+            _tokenService = tokenService;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _config = config;
+        }
+    }
+}
