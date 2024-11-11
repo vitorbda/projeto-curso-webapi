4e67e4670822ac48165f61a5ba59c4f76f748eab
Joao V B Andrade <80553392+vitorbda@users.noreply.github.com>
2024-11-11 09:35:51 -0300

    [S11-A163] Atributo MapToVersion

---
+++ APICatalogo/APICatalogo/Controller/TesteV3Controller.cs
@@ -0,0 +1,27 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controller
+{
+    [Route("api/teste")]
+    [ApiController]
+    [ApiVersion(3)]
+    [ApiVersion(4)]
+    public class TesteV3Controller : ControllerBase
+    {
+
+        [MapToApiVersion(3)]
+        [HttpGet]
+        public string GetVersion3()
+        {
+            return "TesteV3 - GET - Api Versão 3.0";
+        }
+
+        [MapToApiVersion(4)]
+        [HttpGet]
+        public string GetVersion4()
+        {
+            return "TesteV4 - GET - Api Versão 4.0";
+        }
+    }
+}
