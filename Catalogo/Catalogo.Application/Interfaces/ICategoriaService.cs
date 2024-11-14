dcd5ef2c446cd236dce0e91f52f469d15fe7e443
Joao V B Andrade <80553392+vitorbda@users.noreply.github.com>
2024-11-14 15:29:58 -0300

    [S14-A181] Implementação da Clean Architecture

---
+++ Catalogo/Catalogo.Application/Interfaces/ICategoriaService.cs
@@ -0,0 +1,13 @@
+﻿using Catalogo.Application.DTOs;
+
+namespace Catalogo.Application.Interfaces
+{
+    public interface ICategoriaService
+    {
+        Task<IEnumerable<CategoriaDTO>> GetCategorias();
+        Task<CategoriaDTO> GetById(int? id);
+        Task Add(CategoriaDTO categoriaDto);
+        Task Update(CategoriaDTO categoriaDto);
+        Task Remove(int? id);
+    }
+}
