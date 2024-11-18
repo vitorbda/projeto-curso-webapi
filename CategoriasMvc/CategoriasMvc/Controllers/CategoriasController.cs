54da69d79ff77cb6d93b85676434cd02b464e347
Joao V B Andrade <80553392+vitorbda@users.noreply.github.com>
2024-11-18 08:12:07 -0300

    [S16A-191] Criando o Controlador e as Views no projeto MVC

---
+++ CategoriasMvc/CategoriasMvc/Controllers/CategoriasController.cs
@@ -0,0 +1,26 @@
+﻿using CategoriasMvc.Models;
+using CategoriasMvc.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CategoriasMvc.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private readonly ICategoriaService _categoriaService;
+
+        public CategoriasController(ICategoriaService categoriaService)
+        {
+            _categoriaService = categoriaService;
+        }
+
+        public async Task<ActionResult<IEnumerable<CategoriaViewModel>>> Index()
+        {
+            var result = await _categoriaService.GetCategorias();
+
+            if (result is null)
+                return View("Error");
+
+            return View(result);
+        }
+    }
+}
