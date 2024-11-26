129c71fdeab1dc0cd7add3f7f55b8806f59bdded
Joao V B Andrade <80553392+vitorbda@users.noreply.github.com>
2024-11-26 09:38:00 -0300

    [S18-A215] Criando a minimal API - ApiTarefas - III

--- ApiTarefas/ApiTarefas/Program.cs
+++ ApiTarefas/ApiTarefas/Program.cs
@@ -21,19 +21,48 @@ app.MapGet("frases", async () =>
     await new HttpClient().GetStringAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
 );
 
+app.MapGet("/tarefas/{id}", async (int id, AppDbContext db) => await db.Tarefas.FindAsync(id) is Tarefa tarefa ? Results.Ok(tarefa) : Results.NotFound());
 
 app.MapGet("/tarefas", async (AppDbContext db) => await db.Tarefas.ToListAsync());
+
+app.MapGet("/tarefas/concluida", async (AppDbContext db) => await db.Tarefas.Where(t => t.IsConcluida).ToListAsync());
 
 app.MapPost("/tarefas", async (Tarefa tarefa, AppDbContext db) =>
 {
     db.Tarefas.Add(tarefa);
     await db.SaveChangesAsync();
     return Results.Created($"/tarefas/{tarefa.Id}", tarefa);
-}); 
+});
 
+
+
+
+
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    SeedDatabase(dbContext);
+}
 
 app.Run();
 
+void SeedDatabase(AppDbContext db)
+{
+    Random random = new Random();
+
+    for (int i = 0; i <= 10; i++)
+    {
+        db.Tarefas.Add(new Tarefa
+        {
+            Nome = $"Tarefa - {random.Next(100)}",
+            IsConcluida = random.Next(2) == 1
+        });
+    }
+
+    db.SaveChanges();
+}
 
 class Tarefa
 {
@@ -42,10 +71,9 @@ class Tarefa
     public bool IsConcluida { get; set; }
 }
 
-
 class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
 
-    public DbSet<Tarefa> Tarefas => Set<Tarefa>();
+    public DbSet<Tarefa> Tarefas => Set<Tarefa>();   
 }
