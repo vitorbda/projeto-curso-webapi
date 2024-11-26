3e0173187f353356394347a3eadd1640d1b03ea8
Joao V B Andrade <80553392+vitorbda@users.noreply.github.com>
2024-11-26 09:06:18 -0300

    [S18-A214] Criando a minimal API - ApiTarefas - II

--- ApiTarefas/ApiTarefas/Program.cs
+++ ApiTarefas/ApiTarefas/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("TarefasDB"));
 
 var app = builder.Build();
 
@@ -17,4 +21,31 @@ app.MapGet("frases", async () =>
     await new HttpClient().GetStringAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
 );
 
+
+app.MapGet("/tarefas", async (AppDbContext db) => await db.Tarefas.ToListAsync());
+
+app.MapPost("/tarefas", async (Tarefa tarefa, AppDbContext db) =>
+{
+    db.Tarefas.Add(tarefa);
+    await db.SaveChangesAsync();
+    return Results.Created($"/tarefas/{tarefa.Id}", tarefa);
+}); 
+
+
 app.Run();
+
+
+class Tarefa
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public bool IsConcluida { get; set; }
+}
+
+
+class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Tarefa> Tarefas => Set<Tarefa>();
+}
