0f3cc477385269e5285d8d0b5627b4f0138fc34f
Joao V B Andrade <80553392+vitorbda@users.noreply.github.com>
2024-11-27 07:36:00 -0300

    [S18-A218] Criando uma Minimal API usando o Dapper - II

--- TarefasApi/TarefasApi/Program.cs
+++ TarefasApi/TarefasApi/Program.cs
@@ -1,4 +1,9 @@
+using TarefasApi.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.AddPersistence();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
