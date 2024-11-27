0bf4e01c5b6a3d5336495b06ede44a42df111d9a
Joao V B Andrade <80553392+vitorbda@users.noreply.github.com>
2024-11-27 08:39:49 -0300

    [S18-A220] Criando uma Minimal API usando o Dapper - IV

--- TarefasApi/TarefasApi/Endpoints/TarefasEndpoints.cs
+++ TarefasApi/TarefasApi/Endpoints/TarefasEndpoints.cs
@@ -15,7 +15,42 @@ public static class TarefasEndpoints
             using var con = await connectionGetter();
             var tarefas = con.GetAll<Tarefa>().ToList();
 
-            return tarefas is null ? Results.NotFound() : Results.Ok(tarefas);
+            return GenericGetReturn(tarefas);
+        });
+
+        app.MapGet("/tarefas/{id}", async (GetConnection connectionGetter, int id) =>
+        {
+            using var con = await connectionGetter();
+            var tarefa = con.Get<Tarefa>(id);
+            return GenericGetReturn(tarefa);
+        });
+
+        app.MapPost("/tarefas", async (GetConnection connectionGetter, Tarefa tarefa) =>
+        {
+            using var con = await connectionGetter();
+            return Results.Created($"/tarefas/{con.Insert<Tarefa>(tarefa)}", tarefa);
+        });
+
+        app.MapPut("/tarefas", async (GetConnection connectionGetter, Tarefa tarefa) =>
+        {
+            using var con = await connectionGetter();
+            _ = con.Update(tarefa);
+            return Results.Ok();
+        });
+
+        app.MapDelete("/tarefas/{id}", async (GetConnection connectionGetter, int id) =>
+        {
+            using var con = await connectionGetter();
+            var tarefa = con.Get<Tarefa>(id);
+
+            if (tarefa is null) return Results.NotFound();
+
+            con.Delete(tarefa);
+
+            return Results.Ok(tarefa);
         });
     }
+
+    private static IResult GenericGetReturn(object obj) => obj is null ? Results.NotFound() : Results.Ok(obj);
+
 }
