ca22176f0ee132ca20f9b52b4f78811678d6b83f
Joao V B Andrade <80553392+vitorbda@users.noreply.github.com>
2024-12-02 08:58:30 -0300

    [S19-A235] Criando os endpoints para Produtos

--- CatalogoAPI/CatalogoApi/Models/Produto.cs
+++ CatalogoAPI/CatalogoApi/Models/Produto.cs
@@ -1,4 +1,6 @@
-﻿namespace CatalogoApi.Models
+﻿using System.Text.Json.Serialization;
+
+namespace CatalogoApi.Models
 {
     public class Produto
     {
@@ -11,6 +13,7 @@
         public int Estoque { get; set; }
 
         public int CategoriaId { get; set; }
-        public Categoria Categoria { get; set; } = new Categoria();
+        [JsonIgnore]
+        public Categoria Categoria { get; set; }
     }
 }
