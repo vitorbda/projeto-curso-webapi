e31a34987f0f849f4a6260efb0cf94333dbd7b2b
Joao V B Andrade <80553392+vitorbda@users.noreply.github.com>
2024-12-02 07:58:31 -0300

    [S19-A227] Usando a Fluent API para configurar as entidades

--- CatalogoAPI/CatalogoApi/Context/AppDbContext.cs
+++ CatalogoAPI/CatalogoApi/Context/AppDbContext.cs
@@ -11,5 +11,19 @@ namespace CatalogoApi.Context
 
         public DbSet<Produto> Produtos { get; set; }
         public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<Categoria>().HasKey(c => c.Id);
+            mb.Entity<Categoria>().Property(c => c.Nome).HasMaxLength(100).IsRequired();
+            mb.Entity<Categoria>().Property(c => c.Descricao).HasMaxLength(150).IsRequired();
+
+            mb.Entity<Produto>().HasKey(p => p.Id);
+            mb.Entity<Produto>().Property(p => p.Nome).HasMaxLength(100).IsRequired();
+            mb.Entity<Produto>().Property(p => p.Descricao).HasMaxLength(100);
+            mb.Entity<Produto>().Property(p => p.Imagem).HasMaxLength(100);
+            mb.Entity<Produto>().Property(p => p.Preco).HasPrecision(14, 2);
+            mb.Entity<Produto>().HasOne<Categoria>(c => c.Categoria).WithMany(p => p.Produtos).HasForeignKey(c => c.CategoriaId);
+        }
     }
 }
