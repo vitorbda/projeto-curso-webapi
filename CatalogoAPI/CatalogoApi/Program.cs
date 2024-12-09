2012c0c7b20015e2194999f3acef59bdd999a0ea
Joao V B Andrade <80553392+vitorbda@users.noreply.github.com>
2024-12-09 07:47:44 -0300

    [S19-A241] Organizando o código da Minimal API - II

--- CatalogoAPI/CatalogoApi/Program.cs
+++ CatalogoAPI/CatalogoApi/Program.cs
@@ -1,12 +1,11 @@
+using CatalogoApi.ApiEndpoints;
 using CatalogoApi.Context;
-using CatalogoApi.Models;
 using CatalogoApi.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-using System.Text;
 using Microsoft.OpenApi.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -79,140 +78,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-
-////////////////// LOGIN
-
-app.MapPost("/login", [AllowAnonymous] (UserModel userModel, ITokenService tokenService) =>
-{
-    if (userModel is null)    
-        return Results.BadRequest("Login Inválido");
-    
-    if (userModel.UserName == "macoratti" && userModel.Password == "numsey#123")
-    {
-        var tokenString = tokenService.GerarToken(
-            app.Configuration["Jwt:Key"],
-            app.Configuration["Jwt:Issuer"],
-            app.Configuration["Jwt:Audience"],
-            userModel);
-        return Results.Ok(new { token = tokenString });
-    }
-    else
-    {
-        return Results.BadRequest("Login Inválido");
-    }
-}).Produces(StatusCodes.Status400BadRequest)
-              .Produces(StatusCodes.Status200OK)
-              .WithName("Login")
-              .WithTags("Autenticacao");
-
-
-//////////// CATEGORIAS
-
-app.MapGet("/", () => "Catalogo de Produtos - 2024").ExcludeFromDescription();
-
-app.MapPost("/categorias", async (Categoria categoria, AppDbContext db) =>
-{
-    db.Categorias.Add(categoria);
-    await db.SaveChangesAsync();
-
-    return Results.Created($"/categorias/{categoria.Id}", categoria);
-});
-
-app.MapGet("/categorias", async (AppDbContext db) => await db.Categorias.ToListAsync()).WithTags("Categorias").RequireAuthorization();
-
-app.MapGet("/categorias/{id:int}", async (int id, AppDbContext db) =>
-{
-    return await db.Categorias.FindAsync(id)
-        is Categoria categoria
-        ? Results.Ok(categoria)
-        : Results.NotFound();
-});
-
-app.MapPut("/categorias/{id:int}", async(int id, Categoria categoria, AppDbContext db) =>
-{
-    if (categoria.Id != id)
-        return Results.BadRequest();
-
-    var categoriaDb = await db.Categorias.FindAsync(id);
-
-    if (categoriaDb is null)
-        return Results.NotFound();
-
-    categoriaDb.Nome = categoria.Nome;
-    categoriaDb.Descricao = categoria.Descricao;
-
-    await db.SaveChangesAsync();
-    return Results.Ok(categoriaDb);
-});
-
-app.MapDelete("/categorias/{id:int}", async (int id, AppDbContext db) =>
-{
-    var categoria = await db.Categorias.FindAsync(id);
-
-    if (categoria is null)
-        return Results.NotFound();
-
-    db.Categorias.Remove(categoria);
-    await db.SaveChangesAsync();
-
-    return Results.NoContent();
-});
-
-///////////////////////////////// PRODUTOS  
-
-app.MapPost("/produtos", async (Produto produto, AppDbContext db) =>
-{
-    db.Produtos.Add(produto);
-    await db.SaveChangesAsync();
-
-    return Results.Created($"/produtos/{produto.Id}", produto);
-});
-
-app.MapGet("/produtos", async (AppDbContext db) => await db.Produtos.ToListAsync()).WithTags("Produto").RequireAuthorization();
-
-app.MapGet("/produtos/{id:int}", async (int id, AppDbContext db) =>
-{
-    return await db.Produtos.FindAsync(id)
-        is Produto produto
-        ? Results.Ok(produto)
-        : Results.NotFound();
-});
-
-app.MapPut("/produtos/{id:int}", async (int id, Produto produto, AppDbContext db) =>
-{
-    if (produto.Id != id)
-        return Results.BadRequest();
-
-    var produtoDb = await db.Produtos.FindAsync(id);
-
-    if (produtoDb is null)
-        return Results.NotFound();
-
-    produtoDb.Nome = produto.Nome;
-    produtoDb.Descricao = produto.Descricao;
-    produtoDb.Preco = produto.Preco;
-    produtoDb.Imagem = produto.Imagem;
-    produtoDb.DataCompra = produto.DataCompra;
-    produtoDb.Estoque = produto.Estoque;
-    produtoDb.CategoriaId = produto.CategoriaId;
-
-    await db.SaveChangesAsync();
-    return Results.Ok(produtoDb);
-});
-
-app.MapDelete("/produtos/{id:int}", async (int id, AppDbContext db) =>
-{
-    var produto = await db.Produtos.FindAsync(id);
-
-    if (produto is null)
-        return Results.NotFound();
-
-    db.Produtos.Remove(produto);
-    await db.SaveChangesAsync();
-
-    return Results.NoContent();
-});
-
+app.MapAutenticacaoEndpoints();
+app.MapCategoriasEndpoints();
+app.MapProdutosEndpoints();
 
 app.UseHttpsRedirection();
 
