5ee9a5ce855f2fe7fb99e200b2d317aefdc1e8cc
Joao V B Andrade <80553392+vitorbda@users.noreply.github.com>
2024-12-17 08:34:42 -0300

    [S21-A257] Criando a aplicação React clientereact - I

---
+++ AlunosReact/clientreact/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Login from './pages/Login';
+import './Global.css';
+
+function App() {
+  return (
+    <Login />
+  );
+}
+
+export default App;
---
+++ AlunosReact/clientreact/src/pages/Login/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './styles.css';
+import logoImage from '../../assets/login.png';
+
+export default function Login() {
+    return (
+        <div className="login-container">
+            <section className="form">
+
+                <img src={logoImage} alt="Login" id="img1" />
+
+                <form>
+                    <h1>Cadastro de Alunos</h1>
+                    <input placeholder="Email" />
+                    <input type="password" placeholder="Senha" />
+                    <button className="button" type="submit">Login</button>
+                </form>
+
+            </section>
+        </div>
+    )
+}
