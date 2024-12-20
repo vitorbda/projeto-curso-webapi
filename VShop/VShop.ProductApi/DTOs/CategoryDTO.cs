8b9223dd91d1cb4edb40ac3020ec43f7b790158c
João Vitor de Andrade <bdandrade.joaovitor@gmail.com>
2024-12-20 16:51:41 -0300

    [S22-A267] Definindo os DTOs e os repositórios para o microsserviço ProductApi

---
+++ VShop/VShop.ProductApi/DTOs/CategoryDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using VShop.ProductApi.Models;
+
+namespace VShop.ProductApi.DTOs;
+
+public class CategoryDTO
+{
+    public int Id { get; set; }
+    [Required(ErrorMessage = "The name is required")]
+    [StringLength(100, MinimumLength = 3)]
+    public string Name { get; set; }
+    public ICollection<Product> Products { get; set; }
+}
---
+++ VShop/VShop.ProductApi/DTOs/ProductDTO.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using VShop.ProductApi.Models;
+
+namespace VShop.ProductApi.DTOs;
+
+public class ProductDTO
+{
+    public int Id { get; set; }
+    [Required(ErrorMessage = "The name is required")]
+    [StringLength(100, MinimumLength = 3)]
+    public string Name { get; set; }
+    [Required(ErrorMessage = "The price is required")]
+    public decimal Price { get; set; }
+    [Required(ErrorMessage = "The description is required")]
+    [StringLength(200, MinimumLength = 5)]
+    public string Description { get; set; }
+    [Required(ErrorMessage = "The stock is required")]
+    [Range(1, 9999)]
+    public long Stock { get; set; }
+    public string ImageUrl { get; set; }
+
+    public Category Category { get; set; }
+    public int CategoryId { get; set; }
+}
