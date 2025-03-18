4bd45edb8b070d3497aee9c4c430aa0f8bba2a2b
João Vitor de Andrade <bdandrade.joaovitor@gmail.com>
2025-03-18 08:35:05 -0300

    [S22-A279] Exibindo os itens do carrinho de compras

--- VShop/VShop.Web/Program.cs
+++ VShop/VShop.Web/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddHttpClient<ICartService, CartService>("CartApi", c => c.Base
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ICartService, CartService>();
+builder.Services.AddScoped<ICouponService, CouponService>();
 
 
 builder.Services.AddAuthentication(options =>
