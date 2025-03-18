73bc46d07f3a4c1d987d97a8a9d5a89dde5bd934
João Vitor de Andrade <bdandrade.joaovitor@gmail.com>
2025-03-18 08:47:41 -0300

    [S22-A280] Criando o microsserviço para cupom de desconto - DiscountApi

---
+++ VShop/VShop.DiscountApi/Controllers/CouponController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using VShop.DiscountApi.DTOs;
+using VShop.DiscountApi.Repositories;
+
+namespace VShop.DiscountApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CouponController : ControllerBase
+{
+    private ICouponRepository _repository;
+
+    public CouponController(ICouponRepository repository)
+    {
+        _repository = repository;
+    }
+
+    [HttpGet("{couponCode}")]
+    public async Task<ActionResult<CouponDTO>> GetDiscountCouponByCode(string couponCode)
+    {
+        var coupon = await _repository.GetCouponByCode(couponCode);
+
+        return coupon is null
+            ? NotFound($"Coupon Code: {couponCode} not found")
+            : Ok(coupon);
+    }
+}
